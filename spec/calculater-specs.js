4dfa4bc01638a4731ac5dd95266949bb6b811e8b
Nothemba Tsheme <tnothemba@gmail.com>
2019-06-03 11:58:54 +0200

    Updated tdd

--- spec/calculater-specs.js
+++ spec/calculater-specs.js
@@ -2,14 +2,14 @@ var calculator = new Calculator()
 describe("Add",function(){
 it ("should add two numbers", function(){
     let result = calculator.add(0,0);
-     result = calculator.add(calculator.last(),0,0);
+     result = calculator.add(calculator.Last(),0,0);
     expect(result).toBe(0);
 
 
 });
 it ("should be able to add multiple numbers", function(){
     var result = calculator.add(1,2,4);
-     result = calculator.add(calculator.last(),1,2,4)
+     result = calculator.add(calculator.Last(),1,2,4)
     expect(result).toBe(14);
 });
 
@@ -19,12 +19,12 @@ it ("should be able to add multiple numbers", function(){
 describe("Multiply",function(){
     it ("should multiply two numbers", function(){
         let result = calculator.multiply(2,4);
-         result = calculator.multiply(calculator.last(),2,4);
+         result = calculator.multiply(calculator.Last(),2,4);
         expect(result).toBe(64);
     } )
     it ("should multiply as many numbers as possible", function(){
         let result = calculator.multiply(2,5,7);
-         result = calculator.multiply(calculator.last(),2,5,7);
+         result = calculator.multiply(calculator.Last(),2,5,7);
         expect(result).toBe(4900);
     })
     
@@ -33,8 +33,26 @@ describe("Multiply",function(){
 })
 describe("last", function(){
     it ("should remember the last result", function(){
-        let result = calculator.last()
-        expect(result).toBe(calculator.last());
+        let result = calculator.Last()
+        expect(result).toBe(calculator.Last());
 
     })
+})
+describe("Momory slots", function(){
+it ("should remember more stuff", function(){
+    expect(calculator.add(3, 4)).toBe(7);
+    expect(calculator.setSlot(1));
+    expect(calculator.getSlot(1)).toBe(7);
+
+    expect(calculator.add(2,5,7)).toBe(14);
+    expect(calculator.setSlot(2));
+    expect(calculator.getSlot(2)).toBe(14);
+    
+
+    expect(calculator.add(calculator.getSlot(1), 5));
+    expect(calculator.multiply(calculator.getSlot(2), 7));
+    
+})
+
+
 })
--- src/calculater.js
+++ src/calculater.js
@@ -1,42 +1,89 @@
-class Calculator{
-  constructor(){
-    this.result = 0;
-    this.slot = [];
-  }
-  add(){
-    this.result = 0;
-    for (var i =0; i < arguments.length; i++){
-       this.result += arguments[i];
+// class Calculator{
+//   constructor(){
+//     this.arrSlot = [];
+//     var sum = this.result;
+//   }
+//   add(){
+//     this.result = 0;
+//     for (var i =0; i < arguments.length; i++){
+//        this.result += arguments[i];
        
-   }
-   
-    return this.result;
- }
+//    }
+     
+//     return this.result;
+//  }
        
        
-  multiply(){
-   this.result = 1;
-  for (let i = 0; i < arguments.length; i++){
-      this.result *= parseFloat(arguments[i]);
+//   multiply(){
+//    this.result = 1;
+//   for (let i = 0; i < arguments.length; i++){
+//       this.result *= parseFloat(arguments[i]);
              
-    }
+//     }
     
-    return this.result;
- }
-  last(){
-    return this.result;
-  }
+//     return this.result;
+//  }
+//   last(){
+//     return this.result;
+//   }
 
-  setSlot(){
+//   setSlot(number){
+//    this.arrSlot.push(this.last);
+//    return this.arrSlot[number - 1];
 
+//   }
 
-  }
-}
+//   getSlot(number){
+//     return this.arrSlot; 
+//   }
+// }
 // let calculator = new Calculator();
 //console.log(calculator.multiply(1,3));
 //console.log(calculator.multiply(calculator.last(),1,3));
 //console.log(calculator.last());
+class Calculator{
 
+  constructor(){
+     this.slotArr = [];
+
+  }
+
+  add(){
+      let result = 0;
+      for(let i = 0; i < arguments.length; i++){
+          result += parseFloat(arguments[i]);
+          this.last = result;
+  }
+  return result;
+  }
+
+  multiply(){
+      let result = 1;
+
+      for(let i = 0; i < arguments.length; i++){
+          result *= parseFloat(arguments[i]);
+          this.last = result;
+      }
+      return result;
+  }
+  Last(){
+      return this.last;
+  }
+
+
+  setSlot(number){
+
+
+      this.slotArr.push(this.last);
+      return this.slotArr[number - 1];
+
+  }
+  getSlot(number){
+
+      return this.slotArr[number - 1];
+  }
+
+}
 
 
 
